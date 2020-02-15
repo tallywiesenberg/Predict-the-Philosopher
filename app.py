e35fe085b98e7bfff9517d39a4e98dfb579d582b
Tally Wiesenberg <t.j.wies@gmail.com>
2020-02-14 23:18:29 -0500

    NN

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 import pickle
+import spacy
 
 #Initiate app
 app = dash.Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
@@ -20,12 +21,42 @@ app.layout = html.Div(children=[
             id='text-input',
             placeholder='Try out a text sample!',
         ),
+        html.H6('# of Neighbors'),
+        html.Br(),
+        dcc.Dropdown(
+            id='k-drop',
+            options=[{'label':i, 'value':i} for i in range(5, 25, 5)],
+            value=5
+        ),
         html.H6(id='output-message', children='output will go here')
 
     ]),
     html.Br(),
     html.A('Code on github:', href='https://github.com/tallywiesenberg/Predict-the-Philosopher')
 ])
+
+
+#Ineractive callbacks
+
+def get_doc_vectors(words):
+    # converts a doc into a vector
+    nlp = spacy.load("en_core_web_lg")
+    return nlp(words).vector
+
+@app.callback(
+    Output('output-message', 'children'),
+    [Input('text-input', 'value'),
+     Input('k-drop', 'value')]
+    )
+def display_results(text, k):
+    file = open(f'./models/model_k{k}.pkl', 'rb')
+    model = pickle.load(file)
+    file.close()
+    vector = get_doc_vectors(text)
+    new_obs = [vector]
+    pred = model.predict(new_obs)
+    return f'The author most likely to have written your sample, "{text}", is {pred}.'
+
 #Execute the app
 if __name__ == "__main__":
     app.run_server()
