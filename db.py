7266b2a9f3f49c2151d198b3be2d0135292aa06c
Tally Wiesenberg <t.j.wies@gmail.com>
2020-02-26 18:30:00 -0500

    ts

--- db.py
+++ db.py
@@ -8,39 +8,3 @@ import sqlalchemy
 def load_data():
     df = pickle.load(open('./data/dataframe.pkl', 'rb'))
     return df
-#Import pandas CSV into Extracts table
-def populate_table():
-    '''Create and populate postgreSQL table "extracts" and populate with pandas dataframe'''
-
-    #Credentials
-    db = 'vsodlxyb'
-    user = 'vsodlxyb'
-    password = config('DB_PASSWORD')
-    host = 'rajje.db.elephantsql.com'
-
-    #Connect to database
-    conn = psycopg2.connect(
-        dbname=db,
-        user=user,
-        password=password,
-        host=host
-    )
-    curs = conn.cursor()
-    
-    #Create Table
-    curs.execute('''
-    CREATE TABLE IF NOT EXISTS extracts(
-        id serial PRIMARY KEY,
-        extract TEXT,
-        author VARCHAR(8)
-    )
-    ''')
-    conn.close()
-    
-    #Populate table "extracts"
-    engine = sqlalchemy.create_engine('postgres://vsodlxyb:j8-RAO4oL8UbZVlwxuwy6qX7zg_2teoG@rajje.db.elephantsql.com:5432/vsodlxyb')
-    con = engine.connect()
-    table_name = 'extracts'
-    df = load_data()
-    df.to_sql(table_name, con)
-    con.close()
